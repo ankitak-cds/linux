28b16e2e36632a578f991a113299a978be7f1190
ankitak-cds <79127556+ankitak-cds@users.noreply.github.com>
2021-02-23 16:46:01 +0530

    Update parentWait.c

--- parentWait.c
+++ parentWait.c
@@ -10,9 +10,11 @@ int main() {
       system("ls -l");
       sleep(10);
       system("ls -l");
+      printf("\nIn child before Return");
       return 3; //exit status is 3 from child process
    } else {
       sleep(3);
+      printf("\nIn parent before wait");
       wait(&status);
       printf("In parent process: exit status from child is decimal %d, hexa %0x\n", status, status);
    }
